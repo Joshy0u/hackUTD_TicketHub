24ee136dcb82b594846c4ea5653d9ce99b261d77
Joshy0u <joshuajhn8@gmail.com>
2025-11-08 17:03:50 -0600

    added datetime to tables

--- backend/app/routes/tickets.py
+++ backend/app/routes/tickets.py
@@ -1,15 +1,18 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, 
+from datetime import datetime
 
 tickets = Blueprint('tickets', __name__)
 
 open_tickets = [
     {
         'id': 1,
+        'datetime': datetime.now()
         'content': 'stuff',
         'priority': 100
     },
     {
         'id': 2,
+        'datetime': datetime.now()
         'content': 'more stuff',
         'priority': 50
     }
@@ -20,9 +23,10 @@ def upload_ticket():
     data = request.get_json()
     if not data:
         return jsonify({'error': 'No data provided'}), 400
-    
+                        
     open_tickets.append({
         'id': len(open_tickets) + 1,
+        'datetime': datetime:now()
         'content': data.get('content'),
         'priority': data.get('priority')
     })
