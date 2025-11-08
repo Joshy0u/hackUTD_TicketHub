5b05e2772bfc3b58f03a6b6a190984fa4db297f7
Rei <reicareich@gmail.com>
2025-11-08 16:31:01 -0600

    Add simple ticket endpoints

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,15 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
-from .consumer import start_consumer
 
 def make_app():
   app = Flask("Sensor")
 
   CORS(app)
 
-  from .routes.sensors import sensors
-  app.register_blueprint(sensors)
-
-  start_consumer()
+  from .routes.tickets import tickets
+  app.register_blueprint(tickets, url_prefix='/tickets')
 
   return app
--- backend/app/routes/tickets.py
+++ backend/app/routes/tickets.py
@@ -1,3 +1,34 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
-@tickets.route('/upload', methods=['POST'])
+tickets = Blueprint('tickets', __name__)
+
+open_tickets = [
+    {
+        'id': 1,
+        'content': 'stuff',
+        'priority': 100
+    },
+    {
+        'id': 2,
+        'content': 'more stuff',
+        'priority': 50
+    }
+]
+
+@tickets.route('/open', methods=['POST'])
+def upload_ticket():
+    data = request.get_json()
+    if not data:
+        return jsonify({'error': 'No data provided'}), 400
+    
+    open_tickets.append({
+        'id': len(open_tickets) + 1,
+        'content': data.get('content'),
+        'priority': data.get('priority')
+    })
+
+    return jsonify({'message': 'Ticket uploaded successfully'}), 201
+
+@tickets.route('/', methods=['GET'])
+def get_tickets():
+    return jsonify(open_tickets), 200
